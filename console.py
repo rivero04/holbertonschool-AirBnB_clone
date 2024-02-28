c92cb5a05ae844d0a2416abeceb8616cfcee8af6
rivero04 <7710@holbertonstudents.com>
2024-02-28 08:39:40 -0800

    tasks

--- console.py
+++ console.py
@@ -1,21 +1,23 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class"""
     prompt = '(hbnb) '
-    
+
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
-    
+
     def do_EOF(self, arg):
         """EOF command to exit the program"""
         return True
-    
+
     def do_help(self, arg):
+        """Help command to provide assistance"""
         return super().do_help(arg)
 
     def emptyline(self):
-        """Do nothing"""
+        """An empty line + ENTER shouldn’t execute anything"""
         pass
 
 if __name__ == '__main__':
