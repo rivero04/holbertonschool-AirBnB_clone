5831affe1d883d12650c5062016ac3bcd44fda35
rivero04 <7710@holbertonstudents.com>
2024-02-28 04:58:18 -0800

    tasks

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,17 +1,18 @@
 import json
 import os
 
+
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
         return FileStorage.__objects
-    
+
     def new(self, obj):
         key = obj.__class.__name + "." + obj.id
         FileStorage.__objects[key] = obj
-        
+
     def save(self):
         with open(FileStorage.__file_path, 'w') as f:
             serialized_objects = {}
@@ -22,18 +23,17 @@ class FileStorage:
             serialized_objects[key] = dict_object
 
         json.dump(serialized_objects, f)
-        
+
     def reload(self):
         if os.path.exists(FileStorage.__file_path):
             with open(FileStorage.__file_path, "r") as f:
                 objs = json.load(f)
-                
+
             for obj in objs.values():
                 class_name = obj['__class__']
-                
+
                 actual_class = eval(class_name)
-                
+
                 deserialized_obj = actual_class(**obj)
-                
+
                 self.new(deserialized_obj)
-        
--- tests/test_save_reload_base_model.py
+++ tests/test_save_reload_base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from models import storage
-from models.base_model import BaseModel
+from models.base_model2 import BaseModel
 
 all_objs = storage.all()
 print("-- Reloaded objects --")
