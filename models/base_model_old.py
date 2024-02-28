5471600a31c3dba3a830fb830ba5f2651910a4fd
Nacho <146840051+rivero04@users.noreply.github.com>
2024-02-28 09:36:29 -0300

    Tasks For AirBnB

---
+++ models/base_model_old.py
@@ -0,0 +1,21 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = self.created_at.isoformat()
+        dict_copy['updated_at'] = self.updated_at.isoformat()
+        return dict_copy
---
+++ models/engine/file_storage.py
@@ -0,0 +1,39 @@
+import json
+import os
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+    
+    def all(self):
+        return FileStorage.__objects
+    
+    def new(self, obj):
+        key = obj.__class.__name + "." + obj.id
+        FileStorage.__objects[key] = obj
+        
+    def save(self):
+        with open(FileStorage.__file_path, 'w') as f:
+            serialized_objects = {}
+
+        for key, value in FileStorage.__objects.items():
+            dict_object = value.to_dict()
+
+            serialized_objects[key] = dict_object
+
+        json.dump(serialized_objects, f)
+        
+    def reload(self):
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r") as f:
+                objs = json.load(f)
+                
+            for obj in objs.values():
+                class_name = obj['__class__']
+                
+                actual_class = eval(class_name)
+                
+                deserialized_obj = actual_class(**obj)
+                
+                self.new(deserialized_obj)
+        
