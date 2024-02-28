9d25d9f69229a259b0e7b379513a3e8cb2da33bf
rivero04 <7710@holbertonstudents.com>
2024-02-28 09:32:23 -0800

    fix

--- console.py
+++ console.py
@@ -4,10 +4,13 @@ This is the consolation module. Provides a command line interface to interact wi
 """
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """HBNBCommand class"""
     prompt = '(hbnb) '
+    classes = {"BaseModel": BaseModel}
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -24,6 +27,34 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """An empty line + ENTER shouldn’t execute anything"""
         pass
+    
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel, saves it (to the JSON file) and prints the id"""
+        if not arg:
+            print ("** class name missing **")
+        elif arg not in self.classes:
+            print ("* class doesn't exist **")
+        else:
+            newbase = BaseModel()
+            print(newbase.id)
+    
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on the class name and id"""
+        args = arg.split()
+        if not arg:
+            print ("** class name missing **")
+        elif args[0] not in self.classes:
+            print ("* class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id missing **")
+        else:
+            key = args[0] + "." + args[1]
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                print(storage.all()[key])
+                
+    
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
---
+++ models/user.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" User class"""
+
+from base_model import BaseModel
+def user(BaseModel):
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
