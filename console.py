ad5f5a54c3b05012c381a3c92e41836d06bb93c6
rivero04 <7710@holbertonstudents.com>
2024-02-28 08:31:35 -0800

    fix

--- console.py
+++ console.py
@@ -1,11 +1,18 @@
 import cmd
 
-
 class HBNBCommand(cmd.Cmd):
-    prompt = "(hbnb)"
-    def quit(self):
-        print("quit")
-
+    prompt = '(hbnb) '
+    
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+    
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+    
+    def do_help(self, arg):
+        return super().do_help(arg)
 
 
 if __name__ == '__main__':
