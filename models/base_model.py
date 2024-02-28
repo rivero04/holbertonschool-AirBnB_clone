9e4539b824ebe4c33d00e6084e647ca01a701640
rivero04 <7710@holbertonstudents.com>
2024-02-28 08:11:27 -0800

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,5 @@
 import uuid
 from datetime import datetime
-from models.__init__ import storage
 
 
 class BaseModel:
@@ -12,6 +11,7 @@ class BaseModel:
                 if key != '__class__':
                     setattr(self, key, value)
         else:
+            from models.__init__ import storage
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
             storage.new(self)
@@ -20,6 +20,7 @@ class BaseModel:
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        from models.__init__ import storage
         self.updated_at = datetime.now()
         storage.save()
 
