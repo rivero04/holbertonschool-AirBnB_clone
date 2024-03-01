2e3b8fd509adfb54542ba580229f44db169fa948
rivero04 <7710@holbertonstudents.com>
2024-03-01 08:42:04 -0800

    fix?

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,5 @@
 import json
 import os
-from models.base_model import BaseModel
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
 
 
 class FileStorage:
