aa3e45b5ef9c592578a74746d4bbef153a63d43a
rivero04 <7710@holbertonstudents.com>
2024-03-01 06:13:53 -0800

    fix

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,10 @@
+from models.base_model import BaseModel
+from
+import unittest
+
+class TestAmenity(unittest.TestCase):
+
+    instance = Amenity()
+
+    if __name__ == '__main__':
+        unittest.main()
